03a746d68ffcb19971c97dfc8895ea3f15157bd2
zardoz-ilbarbaro <davide.romboli@gmail.com>
2025-12-06 15:41:35 +0100

    update

---
+++ 1-up.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author 	: zardoz-ilbarbaro
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black 
+#tput setaf 1 = red 
+#tput setaf 2 = green
+#tput setaf 3 = yellow 
+#tput setaf 4 = dark blue 
+#tput setaf 5 = purple
+#tput setaf 6 = cyan 
+#tput setaf 7 = gray 
+#tput setaf 8 = light blue
+##################################################################################################################
+
+# checking if I have the latest files from github
+echo "Checking for newer files online first"
+git pull
+
+# reset - commit your changes or stash them before you merge
+# git reset --hard - personal alias - grh
+
+if [[ -f "./repo.sh" ]]; then
+    echo "Found repo.sh, running it..."
+    bash ./repo.sh
+fi
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Committing to the local repository with a message containing and commit text
+
+git commit -m "update"
+
+# Push the local files to github
+
+if grep -q main .git/config; then
+	echo "Using main"
+	git push -u origin main
+fi
+
+if grep -q master .git/config; then
+	echo "Using master"
+	git push -u origin master
+fi
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
---
+++ README.md
@@ -0,0 +1,2 @@
+# Zardoz repository
+
